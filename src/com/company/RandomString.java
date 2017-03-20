e809b190cce505a7fde933ea92cf75e177bdd08a
StudentStups <studentstups@gmail.com>
2017-03-20 21:04:28 +0200

    New

--- src/com/company/RandomString.java
+++ src/com/company/RandomString.java
@@ -37,7 +37,7 @@ public class RandomString {
         return emailAdress;
     }
 
-    private static   int randomNumber(int min, int max){
+    public static  int randomNumber(int min, int max){
         return (int)(Math.random()*(max-min)+min);
     }
 
--- src/com/company/RandomStringTest.java
+++ src/com/company/RandomStringTest.java
@@ -37,5 +37,66 @@ public class RandomStringTest {
         assertEquals(true,test);
     }
 
+    @Test
+    public void getRandomTest(){
+        int rnd = new RandomString().randomNumber(0,2);
+        boolean numberIsCorrect = false;
+        numberIsCorrect = (rnd >= 0 && rnd <=2);
+        assertEquals(true,numberIsCorrect);
+    }
+
+    @Test
+    public void randomStringWithDogTest(){
+        String rnd = new RandomString().randomString();
+        String someEmail = new RandomString().randomStringWithDog(rnd);
+        boolean accE = false;
+        accE = someEmail.contains(rnd);
+        assertEquals(true,accE);
+    }
+
+    @Test
+    public void  randomEmailTest(){
+        int count = 0;
+        String someEmail = new RandomString().randomEmailAdress();
+        char[] chars = someEmail.toCharArray();
+        for(int i = 0; i < chars.length; i++){
+            if (chars[i]=='@'){
+                count++;
+            }
+        }
+        assertEquals(1,count);
+    }
+
+    @Test
+    public void  randomEmailDotTest(){
+        int count = 0;
+        String someEmail = new RandomString().randomEmailAdress();
+        char[] chars = someEmail.toCharArray();
+        for(int i = 0; i < chars.length; i++){
+            if (chars[i]=='.'){
+                count++;
+            }
+        }
+        assertEquals(1,count);
+    }
+
+    @Test
+    public void  randomEmailDotIndexTest(){
+        String someEmail = new RandomString().randomEmailAdress();
+        char[] chars = someEmail.toCharArray();
+        boolean isTrue = false;
+        int indexAt = 0;
+        int indexDot = 0;
+        for(int i = 0; i < chars.length; i++){
+            if (chars[i]=='@'){
+                indexAt = i;
+            }
+            if (chars[i]=='.'){
+                indexDot=i;
+            }
+            isTrue = indexAt < indexDot;
+        }
+        assertEquals(true,isTrue);
+    }
 
 }
